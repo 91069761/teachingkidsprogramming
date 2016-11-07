8a339797886383e7a9d6af3b462b880541863bee
91069761 <91069761@mvusd.net>
2016-11-07 10:57:49 -0800

    Finished 4 Projects

--- section03ifs/ChooseYourOwnAdventure.java
+++ section03ifs/ChooseYourOwnAdventure.java
@@ -43,41 +43,59 @@ public class ChooseYourOwnAdventure
         .askForTextInput("Do you want to pour the ooze into the 'backyard' or 'toilet'?");
     if ("toilet".equalsIgnoreCase(toiletOrBackyard))
     {
+      pourIntoToilet();
     }
-    //            pourIntoToilet (recipe below) --#12.1
-    //            ------------- Recipe for pourIntoToilet --#12.2
-    //               Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#10
-    //               Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#11
-    //               If they answer "yes" --#13.1
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
-    //               Otherwise, if they answer "heck yes" --#13.2
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
-    //               Otherwise, if they answer anything else --#13.3
-    //                    endStory --#16
-    //            ------------- End of pourIntoToilet recipe --#12.3
     else if ("backyard".equalsIgnoreCase(toiletOrBackyard))
     {
+      pourIntoBackyard();
     }
-    //            pourIntoBackyard (recipe below) --#19.1
-    //            ------------- Recipe for pourIntoBackyard --#19.2
-    //                Tell the user "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water." --#17
-    //                Ask the user "As the man starts to prepare you as soup, do you...'Scream' or 'Faint'?" --#18
-    //                If they answer "faint" --#20.1
-    //                        Tell the user "You made a delicious soup! Yum! The end." --#21
-    //                Otherwise, if they answer "scream" --#20.2
-    //                    startStory --#22
-    //                Otherwise, if they answer anything else --#20.3
-    //                    endStory --#23
-    //         ------------- End of pourIntoBackyard recipe --#19.3
-    //         Otherwise, if they answer anything else --#8.3
-    //            endStory --#9
+    else
+    {
+      endStory();
+    }
+  }
+  private static void pourIntoBackyard()
+  {
+    MessageBox.showMessage(
+        "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water.");
+    String screamOrFaint = MessageBox
+        .askForTextInput("As the man starts to prepare you as soup, do you...'Scream' or 'Faint'?");
+    if ("Faint".equalsIgnoreCase(screamOrFaint))
+    {
+      MessageBox.showMessage("You made a delicious soup! Yum! The end.");
+    }
+    else if ("Scream".equalsIgnoreCase(screamOrFaint))
+    {
+      startStory();
+    }
+    else
+    {
+      endStory();
+    }
+  }
+  private static void pourIntoToilet()
+  {
+    MessageBox.showMessage(
+        "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
+    String yesOrHeckYes = MessageBox.askForTextInput("Do you want to train to be a NINJA?   'Yes' or 'HECK YES'?");
+    if ("Yes".equalsIgnoreCase(yesOrHeckYes))
+    {
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    else if ("HECK YES".equalsIgnoreCase(yesOrHeckYes))
+    {
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    else
+    {
+      endStory();
+    }
   }
   private static void wakeUp()
   {
-    //          ------------- Recipe for wakeUp --#2.2
-    //              Tell the user "You wake up and have a boring day. The end." --#1
     MessageBox.showMessage("You woke up and had a boring day. The end.");
-    //          ------------- End of wakeUp recipe --#2.3
   }
   private static void animateStartStory()
   {
